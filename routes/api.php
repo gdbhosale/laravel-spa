dc13e004c2ee2e09a4479c8db6879b88a0f72a27
gdbhosale <gdb.sci123@gmail.com>
2021-05-31 18:26:56 +0530

    Server Side Sanctum Impl Done

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TokenController;
+use App\Http\Controllers\API\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Auth Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/sanctum/token', TokenController::class);
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +28,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
